7641ebd85ff9318f95d37a550c450f5f06cd7063
golmaker <iv7.software@gmail.com>
2022-02-03 22:59:28 -0300

    done collectionCard, add axios lib

--- src/App.js
+++ src/App.js
@@ -2,15 +2,28 @@
 import './App.css';
 import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
 
 function App() {
+  
+  const [punkListData, setPunkListData] = useState([]);
+  useEffect(() => {
+      const getMyNfts = async () => {
+        const openseaData = await axios.get('https://testnets-api.opensea.io/assets?asset_contract_address=0x5A027A1F234a9499a8eAf308a9d318794633d2A8&order_direction=asc') // Address collection contract
+        console.log('OpenSeaData :', openseaData.data.assets); // debug console
+        setPunkListData(openseaData.data.assets);
+      }    
+      return getMyNfts();
+  }, [])
+
   return (
     <div className='app'>
 
         <Header />
         <CollectionCard 
                 id={0} 
-                name={'Bandana Punk'} 
+                name={'King Punk'} 
                 traits={[{'value': 10}]} 
                 image='https://gateway.pinata.cloud/ipfs/QmUyVfNR7XWPBDi5TRP2nmQGcTXqYutu1Bh9cTvYY7TH3R'
         />
--- src/components/CollectionCard.js
+++ src/components/CollectionCard.js
@@ -1,28 +1,24 @@
-import React from 'react';
-import "./CollectionCard.css"
+import React from "react";
+import "./CollectionCard.css";
 import weth from "../assets/weth.png";
 
-const CollectionCard = ({id, name, traits, image}) => {
+const CollectionCard = ({ id, name, traits, image }) => {
   return (
-    <div className='collectionCard'>
-        
-        <img src={image} alt=''/>
-        
-        <div className='details'>
-        
-          <div className='name'>
-              {name} <div className='id'># {id}</div>
-          </div>
-        
+    <div className="collectionCard">
+      <img src={image} alt="" />
+
+      <div className="details">
+        <div className="name">
+          {name} <div className="id"># {id}</div>
         </div>
 
-        <div className='priceContainer'>
-        
-            <img src={weth} className='wethImage' alt='' />
-            <div className='price'>{traits[0]?.value}</div>
+        <div className="priceContainer">
+          <img src={weth} className="wethImage" alt="" />
+          <div className="price">{traits[0]?.value}</div>
         </div>
+      </div>
     </div>
-  )
+  );
 };
 
 export default CollectionCard;
