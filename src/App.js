af533c6b620628ba64d1cba64b3da4e7f4ffa007
golmaker <iv7.software@gmail.com>
2022-02-03 22:01:40 -0300

    build collectionCard

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 /* © BacoFinance 2022 by Baco Group */
 import './App.css';
+import CollectionCard from './components/CollectionCard';
 import Header from './components/Header';
 
 function App() {
@@ -7,6 +8,12 @@ function App() {
     <div className='app'>
 
         <Header />
+        <CollectionCard 
+                id={0} 
+                name={'Bandana Punk'} 
+                traits={[{'value': 10}]} 
+                image='https://gateway.pinata.cloud/ipfs/QmUyVfNR7XWPBDi5TRP2nmQGcTXqYutu1Bh9cTvYY7TH3R'
+        />
     
     </div>
   );
---
+++ src/components/CollectionCard.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import "./CollectionCard.css"
+import weth from "../assets/weth.png";
+
+const CollectionCard = ({id, name, traits, image}) => {
+  return (
+    <div className='collectionCard'>
+        
+        <img src={image} alt=''/>
+        
+        <div className='details'>
+        
+          <div className='name'>
+              {name} <div className='id'># {id}</div>
+          </div>
+        
+        </div>
+
+        <div className='priceContainer'>
+        
+            <img src={weth} className='wethImage' alt='' />
+            <div className='price'>{traits[0]?.value}</div>
+        </div>
+    </div>
+  )
+};
+
+export default CollectionCard;
